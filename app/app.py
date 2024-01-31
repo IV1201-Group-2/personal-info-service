3b8d1a1ded6b04fcad313ead22ba94fe0806dcd8
Azmeer K. <azmeer.khawaja@outlook.com>
2024-01-31 17:18:28 +0100

    Initialize Flask app and extensions, register 'personal_info_bp' blueprint

--- app/app.py
+++ app/app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 
+from app.extensions import database, jwt
+from app.routes.personal_info_routes import personal_info_bp
+
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
 
+database.init_app(app)
+jwt.init_app(app)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+app.register_blueprint(personal_info_bp, url_prefix='/recruitment/personal_info')
 
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
