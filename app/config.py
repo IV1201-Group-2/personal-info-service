e4843009723bd6c041bba35d28bfcc64db62db6f
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-01 17:09:15 +0100

    Update configuration settings in config.py for logging.

--- app/config.py
+++ app/config.py
@@ -2,3 +2,15 @@ import os
 
 JWT_SECRET_KEY = os.environ.get('JWT_SECRET')
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+LOG_LEVEL = os.environ.get('INFO')
+LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+LOG_DIR = os.environ.get('LOG_DIR', 'logs')
+LOG_FILENAME = os.environ.get('LOG_FILENAME', 'app.log')
+LOG_FILE = os.path.join(LOG_DIR, LOG_FILENAME)
+LOG_DISABLE_COLOR = True
+
+if 'DYNO' in os.environ:
+    LOG_TO_STDOUT = True
+else:
+    LOG_TO_STDOUT = False
