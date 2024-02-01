bec2d4694c2f56a771e7dfee7724e11d340ea12d
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-01 17:09:56 +0100

    Improve error handling in 'get_person_from_db' function

--- app/repositories/personal_info_repository.py
+++ app/repositories/personal_info_repository.py
@@ -1,5 +1,20 @@
+from flask import current_app
+from sqlalchemy.exc import NoResultFound, MultipleResultsFound, SQLAlchemyError
+
 from app.models.person import Person
 
 
-def get_person_from_db(username: str) -> Person or None:
-    return Person.query.filter_by(username=username).first()
+def get_person_from_db(username: str) -> Person:
+    try:
+        return Person.query.filter_by(username=username).one()
+    except NoResultFound as exception:
+        log_and_raise(exception, f'No user found with username {username}.')
+    except MultipleResultsFound as exception:
+        log_and_raise(exception, f'Multiple users found with username {username}.')
+    except SQLAlchemyError as exception:
+        log_and_raise(exception, 'Error retrieving user from database.')
+
+
+def log_and_raise(exception, message):
+    current_app.logger.error(exception)
+    raise exception(message)
