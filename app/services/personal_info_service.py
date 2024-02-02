a79f556600d3dc1300c4b476caff66b107d5bc84
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-02 15:45:06 +0100

    Add missing type hints, and improved encapsulation of method.

--- app/services/personal_info_service.py
+++ app/services/personal_info_service.py
@@ -1,17 +1,18 @@
 from sqlalchemy.exc import MultipleResultsFound, SQLAlchemyError, NoResultFound
 
+from app.models.person import Person
 from app.repositories.personal_info_repository import get_person_from_db
 
 
 def fetch_personal_info(username: str) -> dict[str, str]:
     try:
         person = get_person_from_db(username)
-        return person_to_dict(person)
+        return __person_to_dict(person)
     except (NoResultFound, MultipleResultsFound, SQLAlchemyError) as exception:
         raise exception
 
 
-def person_to_dict(person):
+def __person_to_dict(person: Person) -> dict:
     return {
         'person_id': person.person_id,
         'name': person.name,
