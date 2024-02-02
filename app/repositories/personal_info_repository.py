a3a5222b222d32e4da6f6ecdd6315753945b5fb3
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-02 02:10:33 +0100

    Correction of exception handling.

--- app/repositories/personal_info_repository.py
+++ app/repositories/personal_info_repository.py
@@ -7,14 +7,16 @@ from app.models.person import Person
 def get_person_from_db(username: str) -> Person:
     try:
         return Person.query.filter_by(username=username).one()
-    except NoResultFound as exception:
-        log_and_raise(exception, f'No user found with username {username}.')
-    except MultipleResultsFound as exception:
-        log_and_raise(exception, f'Multiple users found with username {username}.')
-    except SQLAlchemyError as exception:
-        log_and_raise(exception, 'Error retrieving user from database.')
+    except NoResultFound:
+        log_and_raise(NoResultFound, f'No user found with username {username}.')
+    except MultipleResultsFound:
+        log_and_raise(MultipleResultsFound,
+                      f'Multiple users found with username {username}.')
+    except SQLAlchemyError:
+        log_and_raise(SQLAlchemyError, 'Error retrieving user from the database.')
 
 
-def log_and_raise(exception, message):
+def log_and_raise(exception_type, message):
+    exception = exception_type(message)
     current_app.logger.error(exception)
-    raise exception(message)
+    raise exception
