6690ad328167c59f8be47568110a21a5888c44ed
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-02 02:10:06 +0100

    Adjustment of config vars.

--- app/config.py
+++ app/config.py
@@ -1,14 +1,13 @@
 import os
 
 JWT_SECRET_KEY = os.environ.get('JWT_SECRET')
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///app.db')
 
-LOG_LEVEL = os.environ.get('INFO')
+LOG_LEVEL = 'INFO'
 LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 LOG_DIR = os.environ.get('LOG_DIR', 'logs')
 LOG_FILENAME = os.environ.get('LOG_FILENAME', 'app.log')
 LOG_FILE = os.path.join(LOG_DIR, LOG_FILENAME)
-LOG_DISABLE_COLOR = True
 
 if 'DYNO' in os.environ:
     LOG_TO_STDOUT = True
