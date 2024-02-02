bfe90e776236cd6a0216c499ed7c783dfc685502
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-02 15:41:49 +0100

    Add missing type hints.

--- app/jwt_handlers.py
+++ app/jwt_handlers.py
@@ -1,9 +1,11 @@
 from flask import jsonify, current_app
+from flask_jwt_extended import JWTManager
+from jwt import InvalidTokenError
 
 
-def register_jwt_handlers(jwt):
+def register_jwt_handlers(jwt: JWTManager) -> None:
     @jwt.invalid_token_loader
-    def invalid_token_callback(error):
+    def invalid_token_callback(error: InvalidTokenError) -> tuple:
         current_app.logger.warning(f'Invalid JWT provided: {error}')
         return jsonify({
             'error': 'Invalid token provided',
@@ -11,7 +13,7 @@ def register_jwt_handlers(jwt):
         }), 401
 
     @jwt.expired_token_loader
-    def expired_token_callback(header, payload):
+    def expired_token_callback(header: dict, payload: dict) -> tuple:
         current_app.logger.warning('Expired JWT token')
         return jsonify({
             'error': 'Token has expired',
@@ -19,7 +21,7 @@ def register_jwt_handlers(jwt):
         }), 401
 
     @jwt.unauthorized_loader
-    def unauthorized_callback(error):
+    def unauthorized_callback(error: str) -> tuple:
         current_app.logger.warning(f'Unauthorized request: {error}')
         return jsonify({
             'error': 'Unauthorized',
