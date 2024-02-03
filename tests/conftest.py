3a1d8284b918878895f2bf94cc8495e0ddda579d
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-03 22:28:22 +0100

    Reformat to be in line with PEP8.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,20 +5,20 @@ from app.app import create_app, database
 
 @pytest.fixture(scope='module')
 def app_with_client():
-    flask_app = create_app(test_config=True)
-    flask_app.config.update({
-        'TESTING': True,
-        'JWT_SECRET_KEY': 'your-test-secret-key',
-        'SQLALCHEMY_DATABASE_URI': 'sqlite:///:memory:',
-    })
+	flask_app = create_app(test_config=True)
+	flask_app.config.update({
+		'TESTING': True,
+		'JWT_SECRET_KEY': 'your-test-secret-key',
+		'SQLALCHEMY_DATABASE_URI': 'sqlite:///:memory:',
+	})
 
-    with flask_app.app_context():
-        database.create_all()
-        database.session.commit()
+	with flask_app.app_context():
+		database.create_all()
+		database.session.commit()
 
-    with flask_app.test_client() as testing_client:
-        yield flask_app, testing_client
+	with flask_app.test_client() as testing_client:
+		yield flask_app, testing_client
 
-    with flask_app.app_context():
-        database.session.remove()
-        database.drop_all()
+	with flask_app.app_context():
+		database.session.remove()
+		database.drop_all()
