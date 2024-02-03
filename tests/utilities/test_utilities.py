ca186d839158c4c8cc2ebc39d27ec9dd584e29fd
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-04 00:41:42 +0100

    Add functions to add and remove competences to/from DB.

--- tests/utilities/test_utilities.py
+++ tests/utilities/test_utilities.py
@@ -3,6 +3,7 @@ import datetime
 from flask_jwt_extended import create_access_token
 
 from app.extensions import database
+from app.models.competence import Competence
 from app.models.person import Person
 
 
@@ -22,3 +23,16 @@ def generate_token_for_user_id_1(app) -> tuple[str, str]:
     with app.app_context():
         return create_access_token(identity={'id': 1},
                                    expires_delta=datetime.timedelta(days=1))
+
+
+def setup_competences_in_db(app):
+    with app.app_context():
+        database.session.add(Competence(name='tester'))
+        database.session.add(Competence(name='developer'))
+        database.session.commit()
+
+
+def remove_competences_from_db(app):
+    with app.app_context():
+        database.session.query(Competence).delete()
+        database.session.commit()
