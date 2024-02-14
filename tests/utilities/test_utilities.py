bae9c8a7e6e3673812b1b9e1cfcf9e83cdb1b7a9
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-14 19:27:19 +0100

    Refactor function name.

--- tests/utilities/test_utilities.py
+++ tests/utilities/test_utilities.py
@@ -7,13 +7,13 @@ from app.models.applicant.person import Person
 from app.models.competence import Competence
 
 
-def setup_test_user_in_db(app):
+def setup_test_user_1_in_db(app):
     with app.app_context():
         database.session.add(Person(name='test', surname='tester', role_id=2))
         database.session.commit()
 
 
-def remove_test_user_from_db(app):
+def remove_test_user_1_from_db(app):
     with app.app_context():
         database.session.query(Person).filter(Person.person_id == 1).delete()
         database.session.commit()
@@ -21,7 +21,8 @@ def remove_test_user_from_db(app):
 
 def generate_token_for_user_id_1(app) -> tuple[str, str]:
     with app.app_context():
-        return create_access_token(identity={'id': 1},
+        return create_access_token(identity=None,
+                                   additional_claims={'id': 1},
                                    expires_delta=datetime.timedelta(days=1))
 
 
