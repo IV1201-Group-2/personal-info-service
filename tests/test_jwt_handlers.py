bf1704649baae15b4e21c431fe067d086d22cbea
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-14 19:28:13 +0100

    Refactor to use utility function and use other token structure.

--- tests/test_jwt_handlers.py
+++ tests/test_jwt_handlers.py
@@ -2,24 +2,21 @@ import datetime
 
 from flask_jwt_extended import create_access_token
 
-from tests.utilities.test_utilities import remove_test_user_from_db, \
-    setup_test_user_in_db
+from tests.utilities.test_utilities import remove_test_user_1_from_db, \
+    setup_test_user_1_in_db, generate_token_for_user_id_1
 
 
 def test_valid_token(app_with_client):
     app, test_client = app_with_client
-    setup_test_user_in_db(app)
-
-    with app.app_context():
-        valid_token = create_access_token(
-                identity={'id': 1}, expires_delta=datetime.timedelta(days=1))
+    setup_test_user_1_in_db(app)
+    valid_token = generate_token_for_user_id_1(app)
 
     response = test_client.get(
-            '/api/application-form/applicant/personal-info/',
-            headers={
-                'Authorization': f'Bearer {valid_token}'})
+        '/api/application-form/applicant/personal-info/',
+        headers={
+            'Authorization': f'Bearer {valid_token}'})
     assert response.status_code == 200
-    remove_test_user_from_db(app)
+    remove_test_user_1_from_db(app)
 
 
 def test_invalid_token(app_with_client):
@@ -27,37 +24,37 @@ def test_invalid_token(app_with_client):
 
     with app.app_context():
         valid_token = create_access_token(
-                identity={'id': 1}, expires_delta=datetime.timedelta(days=1))
+            identity={'id': 1}, expires_delta=datetime.timedelta(days=1))
         invalid_token = valid_token + 'invalid'
 
     response = test_client.get(
-            '/api/application-form/applicant/personal-info/',
-            headers={
-                'Authorization': f'Bearer {invalid_token}'})
+        '/api/application-form/applicant/personal-info/',
+        headers={
+            'Authorization': f'Bearer {invalid_token}'})
     assert response.status_code == 401
     assert response.json['error'] == 'INVALID_TOKEN'
 
 
 def test_expired_token(app_with_client):
     app, test_client = app_with_client
-    setup_test_user_in_db(app)
+    setup_test_user_1_in_db(app)
 
     with app.app_context():
         expired_token = create_access_token(
-                identity={'id': 1}, expires_delta=datetime.timedelta(days=-1))
+            identity={'id': 1}, expires_delta=datetime.timedelta(days=-1))
 
     response = test_client.get(
-            '/api/application-form/applicant/personal-info/',
-            headers={
-                'Authorization': f'Bearer {expired_token}'})
+        '/api/application-form/applicant/personal-info/',
+        headers={
+            'Authorization': f'Bearer {expired_token}'})
     assert response.status_code == 401
     assert response.json['error'] == 'TOKEN_EXPIRED'
-    remove_test_user_from_db(app)
+    remove_test_user_1_from_db(app)
 
 
 def test_unauthorized_request(app_with_client):
     app, test_client = app_with_client
     response = test_client.get(
-            '/api/application-form/applicant/personal-info/')
+        '/api/application-form/applicant/personal-info/')
     assert response.status_code == 401
     assert response.json['error'] == 'UNAUTHORIZED'
