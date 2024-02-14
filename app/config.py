15f416e66788cd36b2b8ee0cd2a99a6d27b32927
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-14 15:58:18 +0100

    Add handling for when provided database URL format is incompatible with the required format.

--- app/config.py
+++ app/config.py
@@ -1,7 +1,13 @@
 import os
 
 JWT_SECRET_KEY = os.environ.get('JWT_SECRET')
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///app.db')
+
+database_url = os.environ.get('DATABASE_URL', 'sqlite:///app.db')
+if database_url.startswith('postgres://'):
+    database_url = database_url.replace(
+        'postgres://', 'postgresql://', 1)
+
+SQLALCHEMY_DATABASE_URI = database_url
 
 LOG_LEVEL = 'INFO'
 LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
