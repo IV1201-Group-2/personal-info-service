0708a641f484dfdf3dc6d38662ae96fda26b66b7
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-18 17:54:26 +0100

    Add functions to add more test users, refactor existing token generator.

--- tests/utilities/test_utilities.py
+++ tests/utilities/test_utilities.py
@@ -17,7 +17,19 @@ def setup_test_user_1_in_db(app):
         database.session.commit()
 
 
-def remove_test_user_1_from_db(app):
+def setup_test_user_2_in_db(app):
+    with app.app_context():
+        database.session.add(Person(name='user2', surname='tester', role_id=2))
+        database.session.commit()
+
+
+def setup_test_user_recruiter_in_db(app):
+    with app.app_context():
+        database.session.add(Person(name='recru', surname='iter', role_id=1))
+        database.session.commit()
+
+
+def remove_users_from_db(app):
     with app.app_context():
         database.session.query(Person).filter(Person.person_id == 1).delete()
         database.session.commit()
@@ -26,7 +38,14 @@ def remove_test_user_1_from_db(app):
 def generate_token_for_person_id_1(app) -> tuple[str, str]:
     with app.app_context():
         return create_access_token(identity=None,
-                                   additional_claims={'id': 1},
+                                   additional_claims={'id': 1, 'role': 2},
+                                   expires_delta=dt.timedelta(days=1))
+
+
+def generate_token_for_recruiter(app) -> tuple[str, str]:
+    with app.app_context():
+        return create_access_token(identity=None,
+                                   additional_claims={'id': 2, 'role': 1},
                                    expires_delta=dt.timedelta(days=1))
 
 
