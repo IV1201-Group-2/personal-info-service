76c90a582e24ea14022b457634ab0557204d7f45
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-18 13:26:14 +0100

    Refactor function names

--- tests/test_jwt_handlers.py
+++ tests/test_jwt_handlers.py
@@ -2,19 +2,19 @@ import datetime
 
 from flask_jwt_extended import create_access_token
 
-from tests.utilities.test_utilities import remove_test_user_1_from_db, \
-    setup_test_user_1_in_db, generate_token_for_user_id_1
+from tests.utilities.test_utilities import generate_token_for_person_id_1, \
+    remove_test_user_1_from_db, setup_test_user_1_in_db
 
 
 def test_valid_token(app_with_client):
     app, test_client = app_with_client
     setup_test_user_1_in_db(app)
-    valid_token = generate_token_for_user_id_1(app)
+    valid_token = generate_token_for_person_id_1(app)
 
     response = test_client.get(
-        '/api/application-form/applicant/personal-info/',
-        headers={
-            'Authorization': f'Bearer {valid_token}'})
+            '/api/application-form/applicant/personal-info/',
+            headers={
+                'Authorization': f'Bearer {valid_token}'})
     assert response.status_code == 200
     remove_test_user_1_from_db(app)
 
@@ -22,13 +22,13 @@ def test_valid_token(app_with_client):
 def test_invalid_token(app_with_client):
     app, test_client = app_with_client
     setup_test_user_1_in_db(app)
-    valid_token = generate_token_for_user_id_1(app)
+    valid_token = generate_token_for_person_id_1(app)
     invalid_token = valid_token + 'invalid'
 
     response = test_client.get(
-        '/api/application-form/applicant/personal-info/',
-        headers={
-            'Authorization': f'Bearer {invalid_token}'})
+            '/api/application-form/applicant/personal-info/',
+            headers={
+                'Authorization': f'Bearer {invalid_token}'})
     assert response.status_code == 401
     assert response.json['error'] == 'INVALID_TOKEN'
 
@@ -39,14 +39,14 @@ def test_expired_token(app_with_client):
 
     with app.app_context():
         expired_token = create_access_token(
-            identity=None,
-            additional_claims={'id': 1},
-            expires_delta=datetime.timedelta(days=-1))
+                identity=None,
+                additional_claims={'id': 1},
+                expires_delta=datetime.timedelta(days=-1))
 
     response = test_client.get(
-        '/api/application-form/applicant/personal-info/',
-        headers={
-            'Authorization': f'Bearer {expired_token}'})
+            '/api/application-form/applicant/personal-info/',
+            headers={
+                'Authorization': f'Bearer {expired_token}'})
     assert response.status_code == 401
     assert response.json['error'] == 'TOKEN_EXPIRED'
     remove_test_user_1_from_db(app)
@@ -55,6 +55,6 @@ def test_expired_token(app_with_client):
 def test_unauthorized_request(app_with_client):
     app, test_client = app_with_client
     response = test_client.get(
-        '/api/application-form/applicant/personal-info/')
+            '/api/application-form/applicant/personal-info/')
     assert response.status_code == 401
     assert response.json['error'] == 'UNAUTHORIZED'
