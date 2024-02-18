cbf3b617de5d7dd2dd6699a3a1adab4d121fccf1
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-18 17:55:01 +0100

    Add tests for secondary endpoint URL.

--- tests/routes/test_personal_info_route.py
+++ tests/routes/test_personal_info_route.py
@@ -4,8 +4,9 @@ from sqlalchemy.exc import SQLAlchemyError
 
 from tests.utilities.test_status_codes import StatusCodes
 from tests.utilities.test_utilities import generate_token_for_person_id_1, \
-    remove_test_user_1_from_db, \
-    setup_test_user_1_in_db
+    generate_token_for_recruiter, remove_users_from_db, \
+    setup_test_user_1_in_db, \
+    setup_test_user_2_in_db, setup_test_user_recruiter_in_db
 
 
 def test_get_personal_info_success(app_with_client):
@@ -14,22 +15,67 @@ def test_get_personal_info_success(app_with_client):
     token = generate_token_for_person_id_1(app)
 
     response = test_client.get(
-            '/api/application-form/applicant/personal-info/',
+            '/api/applicant/personal-info/',
             headers={'Authorization': f'Bearer {token}'})
     assert response.status_code == StatusCodes.OK
     assert response.json['name'] == 'test'
     assert response.json['surname'] == 'tester'
     assert response.json['role'] == 2
 
-    remove_test_user_1_from_db(app)
+    remove_users_from_db(app)
+
+
+def test_get_personal_info_recruiter_success(app_with_client):
+    app, test_client = app_with_client
+    setup_test_user_1_in_db(app)
+    setup_test_user_recruiter_in_db(app)
+    token = generate_token_for_recruiter(app)
+
+    response = test_client.get(
+            '/api/applicant/personal-info/1',
+            headers={'Authorization': f'Bearer {token}'})
+    assert response.status_code == StatusCodes.OK
+    assert response.json['name'] == 'test'
+    assert response.json['surname'] == 'tester'
+    assert response.json['role'] == 2
+
+    remove_users_from_db(app)
+
+
+def test_get_personal_info_unauthorized_access(app_with_client):
+    app, test_client = app_with_client
+    setup_test_user_1_in_db(app)
+    setup_test_user_2_in_db(app)
+    token = generate_token_for_person_id_1(app)
+
+    response = test_client.get(
+            '/api/applicant/personal-info/2',
+            headers={'Authorization': f'Bearer {token}'})
+    assert response.status_code == StatusCodes.UNAUTHORIZED
+
+    remove_users_from_db(app)
+
+
+def test_get_recruiter_unauthorized_access(app_with_client):
+    app, test_client = app_with_client
+    setup_test_user_recruiter_in_db(app)
+    token = generate_token_for_recruiter(app)
+
+    response = test_client.get(
+            '/api/applicant/personal-info/2',
+            headers={'Authorization': f'Bearer {token}'})
+    assert response.status_code == StatusCodes.FORBIDDEN
+
+    remove_users_from_db(app)
 
 
 def test_get_personal_info_no_result(app_with_client):
     app, test_client = app_with_client
+    remove_users_from_db(app)
     token = generate_token_for_person_id_1(app)
 
     response = test_client.get(
-            '/api/application-form/applicant/personal-info/',
+            '/api/applicant/personal-info/',
             headers={'Authorization': f'Bearer {token}'})
     assert response.status_code == StatusCodes.NOT_FOUND
     assert response.json['error'] == 'USER_NOT_FOUND'
@@ -43,7 +89,7 @@ def test_get_personal_info_sqlalchemy_error(mock_fetch, app_with_client):
     mock_fetch.side_effect = SQLAlchemyError("A database error occurred")
 
     response = test_client.get(
-            '/api/application-form/applicant/personal-info/',
+            '/api/applicant/personal-info/',
             headers={'Authorization': f'Bearer {token}'})
 
     assert response.status_code == StatusCodes.INTERNAL_SERVER_ERROR
