d6212de5a60bb7a27f3cc21f33a3dc9d350a0826
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-18 19:31:24 +0100

    Remove redundant code from migration.

--- tests/utilities/test_utilities.py
+++ tests/utilities/test_utilities.py
@@ -4,10 +4,6 @@ from datetime import datetime
 from flask_jwt_extended import create_access_token
 
 from app.extensions import database
-from app.models.application import ApplicationStatus
-from app.models.availability import Availability
-from app.models.competence import Competence
-from app.models.competence_profile import CompetenceProfile
 from app.models.person import Person
 
 
@@ -47,44 +43,3 @@ def generate_token_for_recruiter(app) -> tuple[str, str]:
         return create_access_token(identity=None,
                                    additional_claims={'id': 2, 'role': 1},
                                    expires_delta=dt.timedelta(days=1))
-
-
-def setup_competences_in_db(app):
-    with app.app_context():
-        database.session.add(Competence(name='tester'))
-        database.session.add(Competence(name='developer'))
-        database.session.commit()
-
-
-def remove_competences_from_db(app):
-    with app.app_context():
-        database.session.query(Competence).delete()
-        database.session.commit()
-
-
-def generate_competences() -> list[CompetenceProfile]:
-    return [
-        CompetenceProfile(competence_id=1, person_id=1, years_of_experience=5),
-        CompetenceProfile(competence_id=2, person_id=1, years_of_experience=3)]
-
-
-def generate_availabilities() -> list[Availability]:
-    return [
-        Availability(person_id=1,
-                     from_date=datetime.strptime('2021-01-01', '%Y-%m-%d'),
-                     to_date=datetime.strptime('2021-01-02', '%Y-%m-%d')),
-        Availability(person_id=1,
-                     from_date=datetime.strptime('2021-01-03', '%Y-%m-%d'),
-                     to_date=datetime.strptime('2021-01-04', '%Y-%m-%d'))]
-
-
-def generate_application_status() -> ApplicationStatus:
-    return ApplicationStatus(person_id=1)
-
-
-def remove_application_components_from_db(app):
-    with app.app_context():
-        ApplicationStatus.query.delete()
-        Availability.query.delete()
-        CompetenceProfile.query.delete()
-        database.session.commit()
